9988048e6fdd212acf85922e88830b6a62b18f1f
khaopoon2543 <khaopoon2543@gmail.com>
2020-06-07 15:54:04 +0700

    แก้dashboardคลิ้กเดียวววว

--- dashboard_export.php
+++ dashboard_export.php
@@ -8,7 +8,6 @@
 	// import the PhpSpreadsheet Class
 	use PhpOffice\PhpSpreadsheet\Spreadsheet;
 	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-    $output = '';
     if(isset($_POST["export"])) {
 
         $query = "  SELECT c.* , COUNT(DISTINCT ss.student_ID) as total_student
@@ -23,7 +22,7 @@
 
 			$spreadsheet = new Spreadsheet();
 			$spreadsheet->setActiveSheetIndex(0);
-			$sheet = $spreadsheet->getActiveSheet();
+			$sheet = $spreadsheet->getActiveSheet()->setTitle('แบ่งตามรายวิชา');
 			 
 			$sheet->setCellValue('A1', 'ลำดับที่'); // กำหนดค่าใน cell A1
 			$sheet->setCellValue('B1', "รหัสรายวิชา"); // กำหนดค่าใน cell B1
@@ -52,13 +51,14 @@
             while($rs = mysqli_fetch_array($result)){ 
                 $total_student_request = $rs['total_student_request']; 
             }
-			$sheet->setCellValue("C$last_change_int_to_st","จำนวนการขอเพิ่มรายวิชาทั้งหมด");
+			$sheet->setCellValue("B$last_change_int_to_st","จำนวนทั้งหมด");
 			$sheet->setCellValue("D$last_change_int_to_st",$total_student_request);
+			$sheet->mergeCells("B$last_change_int_to_st:C$last_change_int_to_st");
 			
 			$sheet2 = $spreadsheet->createSheet();
 			// Zero based, so set the second tab as active sheet
-			//$spreadsheet->setActiveSheetIndex(1)
-			//$spreadsheet->getActiveSheet()->setTitle('Second tab');
+			$sheet2 = $spreadsheet->setActiveSheetIndex(1);
+			$sheet2 = $spreadsheet->getActiveSheet()->setTitle('แบ่งตามภาควิชา');
 			$sheet2->setCellValue('A1', 'ลำดับที่'); // กำหนดค่าใน cell A1
 			$sheet2->setCellValue('B1', "ภาควิชา"); // กำหนดค่าใน cell B1
 			$sheet2->setCellValue('C1', "จำนวนนิสิตที่ได้รับอนุมัติเพิ่มรายวิชา");
@@ -87,46 +87,15 @@
             while($rs = mysqli_fetch_array($result)){ 
                 $total_student_request = $rs['total_student_request']; 
             }
-			$sheet2->setCellValue("B$last_change_int_to_st","จำนวนการขอเพิ่มรายวิชาทั้งหมด");
+			$sheet2->setCellValue("B$last_change_int_to_st","จำนวนทั้งหมด");
 			$sheet2->setCellValue("C$last_change_int_to_st",$total_student_request);
 			
-			
-			$writer = new Xlsx($spreadsheet);
-			$output_file = "dashboard.xlsx"; // กำหนดชื่อไฟล์ excel ที่ต้องการ
-			$writer->save($output_file); // สร้าง excel 
-			/*return $output_file*/
-			 
-			if(file_exists($output_file)){ // ตรวจสอบว่ามีไฟล์ หรือมีการสร้างไฟล์ แล้วหรือไม่*/
-				echo '<a href="'.$output_file.'" target="_blank">Download</a>';
-			}
-			/*('Content-Type: application/xlsx');
-            header('Content-Disposition: attachment; filename=dashboard.xlsx');
-            echo $output_file*/
-			/*$streamedResponse->setStatusCode(Response::HTTP_OK);
-			$streamedResponse->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-			$streamedResponse->headers->set('Content-Disposition', 'attachment; filename="your_file.xlsx"');
+			header('Content-Type: application/vnd.ms-excel');
+			header('Content-Disposition: attachment;filename="สรุปผลระบบเพิ่มรายวิชา.xlsx"');
+			//header('Cache-Control: max-age=0');
 
-			return $streamedResponse->send();`*/
-			/*$streamedResponse = new StreamedResponse();
-			$streamedResponse->setCallback(function () use ($spreadsheet) {
-		      // $spreadsheet = //create you spreadsheet here;
-		      $writer =  new Xlsx($spreadsheet);
-		      $writer->save('php://output');
-			});
-
-			$streamedResponse->setStatusCode(200);
-			$streamedResponse->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-			$streamedResponse->headers->set('Content-Disposition', 'attachment; filename="your_file.xls"');
-			return $streamedResponse->send();*/
-			/*$response = response()->streamDownload(function() use ($spreadsheet) {
 			$writer = new Xlsx($spreadsheet);
 			$writer->save('php://output');
-			});
-			$response->setStatusCode(200);
-			$response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-			$response->headers->set('Content-Disposition', 'attachment; filename="your_file.xls"');
-			$response->send();*/
-
 		}
 	}
 ?>
